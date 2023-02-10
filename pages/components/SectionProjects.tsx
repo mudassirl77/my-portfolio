d5630130e187fbefef072b058c071c39dda96d4e
mudassirl77 <mudassir.lallmahamood@gmail.com>
2023-02-10 21:46:31 +0400

    Minor refactoring of SectionProjects

--- pages/components/SectionProjects.tsx
+++ pages/components/SectionProjects.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Image from 'next/image';
+import Image, { StaticImageData } from 'next/image';
 import web1 from '../../public/web1.png';
 import web2 from '../../public/web2.png';
 import web3 from '../../public/web3.png';
@@ -8,7 +8,21 @@ import web5 from '../../public/web5.png';
 import web6 from '../../public/web6.png';
 import Project from './Project';
 
+export interface Project {
+  id: number;
+  image: StaticImageData;
+  alt?: string;
+}
+
 function SectionProjects() {
+  const projects: Project[] = [
+    { id: 1, image: web1, alt: 'web1' },
+    { id: 2, image: web2, alt: 'web2' },
+    { id: 3, image: web3, alt: 'web3' },
+    { id: 4, image: web4, alt: 'web4' },
+    { id: 5, image: web5, alt: 'web5' },
+    { id: 6, image: web6, alt: 'web6' },
+  ];
   return (
     <>
       <section>
@@ -19,12 +33,9 @@ function SectionProjects() {
           </p>
         </div>
         <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
-          <Project src={web1} alt={'web1'} />
-          <Project src={web2} alt={'web2'} />
-          <Project src={web3} alt={'web3'} />
-          <Project src={web4} alt={'web4'} />
-          <Project src={web5} alt={'web5'} />
-          <Project src={web6} alt={'web6'} />
+          {projects.map((project) => (
+            <Project key={project.id} src={project.image} alt={project.alt} />
+          ))}
         </div>
       </section>
     </>
